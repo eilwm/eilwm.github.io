a39f83bc1febcceb9b54ee7b65ccb52ce8814af3
Kshitij Anand <kshita1@uci.edu>
2025-10-05 12:26:12 -0700

    Add KaTeX math support (remark-marh + rehype-katex)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
 export default defineConfig({
   site: 'https://eilwm.github.io',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] }), sitemap()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
